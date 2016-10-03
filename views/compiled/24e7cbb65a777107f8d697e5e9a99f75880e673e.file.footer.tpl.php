35fffb537937c1cd886f206a4c2073dd3eb029e7
FrankVanDerVelde <vrankvdv1998@gmail.com>
2016-10-03 21:31:05 +0200

    Upload start
    .

---
+++ views/compiled/24e7cbb65a777107f8d697e5e9a99f75880e673e.file.footer.tpl.php
@@ -0,0 +1,29 @@
+<?php /* Smarty version Smarty-3.1.18, created on 2016-09-28 16:15:10
+         compiled from "views\footer.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:2776457ebd06e7979a6-43542342%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '24e7cbb65a777107f8d697e5e9a99f75880e673e' => 
+    array (
+      0 => 'views\\footer.tpl',
+      1 => 1475072057,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '2776457ebd06e7979a6-43542342',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.18',
+  'unifunc' => 'content_57ebd06e7bff94_96746712',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_57ebd06e7bff94_96746712')) {function content_57ebd06e7bff94_96746712($_smarty_tpl) {?><footer>
+ <p>Justin houd van god joey van botermessen en mauro van Romy</p>
+
+
+
+</footer>
+</body>
+</html><?php }} ?>
---
+++ views/footer.tpl
@@ -0,0 +1,8 @@
+<footer>
+ <p>Justin houd van god joey van botermessen en mauro van Romy</p>
+
+
+
+</footer>
+</body>
+</html>
